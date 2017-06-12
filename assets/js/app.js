40c03b59056080673f2acfbec9a9cc77b9451d79
Ayda Sulca <suni_yuri_94@hotmail.com>
2017-06-12 05:58:38 -0500

    photos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,46 @@
+'use strict';
+/*
+$("select").on("change", (e) => {
+  e.preventDefault();
+  const option = $('#select').val();
+  const peru = $('#photos-Peru').val();
+  const mexico = $('#photos-Mexico').val();
 
+  if(option == '#photos-Peru'){
+    '#photos-Mexico'.addClass("none");
+    '#photos-Peru'.toggleClass("none");
+  }else {
+    '#photos-Peru'.addClass("none");
+    '#photos-Mexico'.toggleClass("none");
+  }
+})
+*/
+$(function(){
+   const photoPeru= $('#photos-Peru');
+   const coderDiv = $('<div class="coderDiv"></div>');
+
+   photoPeru.append(coderDiv);
+
+   for(var i=0; i<peru.length; i++){
+     const img = $('<img class="photo-coder"></img>');
+     img.attr("src", 'assets/fotos/peru/' + peru[i].image);
+     img.attr("alt",peru[i].name);
+     coderDiv.append(img);
+   }
+   return photoPer;
+});
+
+$(function(){
+   const photoMexico= $('#photos-Mexico');
+   const coderDiv = $('<div class="coderDiv"></div>');
+
+   photoMexico.append(coderDiv);
+
+   for(var i=0; i<mexico.length; i++){
+     const img = $('<img class="photo-coder"></img>');
+     img.attr("src", 'assets/fotos/mexico/' + mexico[i].image);
+    img.attr("alt",mexico[i].name);
+     coderDiv.append(img);
+   }
+   return photoMexico;
+});
