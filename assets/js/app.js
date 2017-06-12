28f3bbec01a92b23bfb202ebeca4f646e6289a98
Ayda Sulca <suni_yuri_94@hotmail.com>
2017-06-12 10:29:39 -0500

    recorrer imagenes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,46 +1,38 @@
 'use strict';
-/*
+const i = 0;
+
 $("select").on("change", (e) => {
   e.preventDefault();
   const option = $('#select').val();
-  const peru = $('#photos-Peru').val();
-  const mexico = $('#photos-Mexico').val();
-
-  if(option == '#photos-Peru'){
-    '#photos-Mexico'.addClass("none");
-    '#photos-Peru'.toggleClass("none");
+  if(option == "peru"){
+    $('#photos-Mexico').addClass("none");
+    $('#photos-Peru').toggleClass("none");
   }else {
-    '#photos-Peru'.addClass("none");
-    '#photos-Mexico'.toggleClass("none");
+    $('#photos-Peru').addClass("none");
+    $('#photos-Mexico').toggleClass("none");
   }
 })
-*/
+
 $(function(){
    const photoPeru= $('#photos-Peru');
-   const coderDiv = $('<div class="coderDiv"></div>');
-
-   photoPeru.append(coderDiv);
-
-   for(var i=0; i<peru.length; i++){
-     const img = $('<img class="photo-coder"></img>');
+   const coderDiv = $('<div class="coderDiv col-md-pull-12"></div>');
+   const img = $('<img class="photo-coder col-md-12"></img>');
      img.attr("src", 'assets/fotos/peru/' + peru[i].image);
      img.attr("alt",peru[i].name);
+     photoPeru.append(coderDiv);
      coderDiv.append(img);
-   }
-   return photoPer;
-});
+   return photoPeru;
+ });
+
+
 
 $(function(){
    const photoMexico= $('#photos-Mexico');
-   const coderDiv = $('<div class="coderDiv"></div>');
-
-   photoMexico.append(coderDiv);
-
-   for(var i=0; i<mexico.length; i++){
-     const img = $('<img class="photo-coder"></img>');
+   const coderDiv = $('<div class="coderDiv col-md-12"></div>');
+     const img = $('<img class="photo-coder col-md-12"></img>');
      img.attr("src", 'assets/fotos/mexico/' + mexico[i].image);
-    img.attr("alt",mexico[i].name);
+     img.attr("alt",mexico[i].name);
+     photoMexico.append(coderDiv);
      coderDiv.append(img);
-   }
    return photoMexico;
-});
+ });
